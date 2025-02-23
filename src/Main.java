f4ce9bb276e0108e4b1b2b2d14cfe92bc1ca49c8
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2025-02-23 23:57:20 +0700

    feat: main class

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Masukkan nama file: ");
+        Scanner sc = new Scanner(System.in);
+        String filename = sc.nextLine();
+        try {
+            FileReader reader = new FileReader("../test/" + filename);
+
+            Board board = reader.readBoard();
+
+            List<Piece> pieces = reader.readPieces();
+
+            long startTime = System.currentTimeMillis(); 
+
+            Solver solver = new Solver(board, pieces);  
+            Boolean solved = solver.solve(0);
+            long endTime = System.currentTimeMillis();  
+            long elapsedTime = endTime - startTime;     
+            if (solved) {
+                System.out.println("Solution found:");
+                board.printBoard();
+                System.out.println("Waktu pencarian: " + elapsedTime + " ms");
+                System.out.println("Banyak kasus yang ditinjau: " + solver.getIterationCount());
+                System.out.println("Apakah anda ingin menyimpan solusi? (ya/tidak)");
+                String save = sc.nextLine();
+                if(save.equals("ya")){
+                    PuzzleImageSaver.savePuzzleImage(board, "../test/" + filename.substring(0,filename.lastIndexOf(".")) + "_solution.png");
+                }
+            } else {
+                System.out.println("No solution found.");
+                System.out.println("Waktu pencarian: " + elapsedTime + " ms");
+                System.out.println("Banyak kasus yang ditinjau: " + solver.getIterationCount());
+            }
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+}
