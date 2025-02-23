2f517490bfb040c8b76a97361748eafb1b3ef704
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2025-02-23 23:56:52 +0700

    feat: filereader class

---
+++ src/FileReader.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileReader {
+    private Scanner sc;
+
+    public FileReader(String filename) throws FileNotFoundException {
+        sc = new Scanner(new File(filename));
+    }
+
+    public Board readBoard() {
+        // Read board dimensions
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int P = sc.nextInt();
+        sc.nextLine(); // Consume the remaining line
+
+        // Read board type
+        String boardType = sc.nextLine();
+        char[][] boardGrid = new char[N][M];
+
+        switch (boardType) {
+            case "DEFAULT" -> {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        boardGrid[i][j] = '*'; // Default fill
+                    }
+                }
+            }
+            case "CUSTOM" -> { 
+                for (int i = 0; i < N; i++) {
+                    String line = sc.nextLine();
+                    for (int j = 0; j < M; j++) {
+                        boardGrid[i][j] = line.charAt(j);
+                        if(boardGrid[i][j] == 'X'){
+                            boardGrid[i][j] = '*';
+                        }   
+                    }
+                }
+            }
+            default -> {
+                System.out.println("Invalid board type!");
+                return null;
+            }
+        }
+
+        return new Board(boardGrid, P);
+    }
+
+    public List<Piece> readPieces() {
+        List<Piece> pieces = new ArrayList<>();
+        List<String> currentPiece = new ArrayList<>();
+        char currentPieceChar = ' ';
+
+        while (sc.hasNextLine()) {
+            String tempLine = sc.nextLine();
+            if (tempLine.isEmpty()) break; // Empty line = end of input
+
+            int idx = 0;
+            int idx2 = tempLine.length();
+            char tempChar = ' ';
+            while((tempChar == ' ') && (idx < idx2)){
+                tempChar = tempLine.charAt(idx);
+                idx++;
+            }   
+            if(tempChar == ' '){
+                break;
+            }
+            if (currentPieceChar == tempChar) {
+                currentPiece.add(tempLine);
+            } else {
+                if (!currentPiece.isEmpty()) {
+                    pieces.add(new Piece(currentPiece));
+                    currentPiece.clear();
+                }
+                currentPieceChar = tempChar;
+                currentPiece.add(tempLine);
+            }
+        }
+
+        if (!currentPiece.isEmpty()) {
+            pieces.add(new Piece(currentPiece));
+        }
+
+        return pieces;
+    }
+
+    public void close() {
+        sc.close();
+    }
+}
