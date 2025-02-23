d4ef39fd273104b7c06565e6dc4c7686ca6d26b6
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2025-02-23 23:56:28 +0700

    feat: piece class

---
+++ src/Piece.java
@@ -0,0 +1,69 @@
+
+import java.util.List;
+
+
+public class Piece {
+    private char[][] shape;
+    private static int pieceCount = 0;
+    
+    public Piece(List<String> shape) {
+        int rows = shape.size();
+        int cols = 0;
+        for (String line : shape) {
+            if (line.length() > cols) {
+                cols = line.length();
+            }
+        }
+
+        this.shape = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            String line = shape.get(i);
+            for (int j = 0; j < cols; j++) {
+                this.shape[i][j] = (j < line.length()) ? line.charAt(j) : ' ';
+            }
+        }
+        pieceCount++;
+    }
+
+    public static int getPieceCount() {
+        return pieceCount;
+    }
+    public char[][] getShape() {
+        return shape;
+    }
+
+    // Rotate the piece 90 degrees clockwise
+    public void rotateClockwise() {
+        int rows = shape.length;
+        int cols = shape[0].length;
+        char[][] rotated = new char[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotated[j][rows - 1 - i] = shape[i][j];
+            }
+        }
+        shape = rotated;
+    }
+
+    // Mirror the piece horizontally
+    public void mirrorHorizontal() {
+        int rows = shape.length;
+        int cols = shape[0].length;
+        char[][] mirrored = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                mirrored[i][cols - 1 - j] = shape[i][j];
+            }
+        }
+        shape = mirrored;
+    }
+    // Print the piece for debugging
+    public void printPiece() {
+        for (char[] row : shape) {
+            System.out.println(new String(row));
+        }
+    }
+}
